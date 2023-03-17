0bba3a74c16c911715d76def7e8c2b89da5c9db8
APapanik09 <89647801+APapanik09@users.noreply.github.com>
2023-03-17 00:16:57 -0400

    Finished project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+if(document.getElementsByClassName("swiper-screenshots")){
 
 const swiper = new Swiper('.swiper-screenshots', {
     loop: true,
@@ -12,3 +13,27 @@ const swiper = new Swiper('.swiper-screenshots', {
       prevEl: '.swiper-button-prev',
     },
   });
+}
+  //Heart button
+if(document.getElementById("heart")){
+  const heart = document.getElementById("heart");
+
+  heart.addEventListener("click", function(){
+    heart.innerHTML = '<i style="color:red;"class="bi bi-chat-heart-fill"></i>';
+  })}
+
+  //App filter swiper
+
+  const swiperApp = new Swiper('.swiper-filters', {
+    grabCursor: true,
+    slidesPerView: 4,
+    spaceBetween: 10,
+  });
+
+  const swiperArt = new Swiper('.swiper-featured-artworks',{
+    grabCursor: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+  })
+
+
