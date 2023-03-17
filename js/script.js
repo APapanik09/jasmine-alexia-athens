f61b0217a0e4447b81182e41b0f210491e3a8045
Jasmine-Lapierre <2188895@cmontmorency.qc.ca>
2023-03-16 23:58:34 -0400

    telephone+swiper LP

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@
 const swiper = new Swiper('.swiper-screenshots', {
     loop: true,
   
-    pagination: {
-
-    },
-  
+  slidesPerView:1,
+  centeredSlides:true,  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints:{
+      1200:{
+        slidesPerView:3,
+        spaceBetween:30,
+      }
+    }
   });
